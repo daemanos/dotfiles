e7f4f76443bee05fb85bffd99e279f8573789f02
Octaplex <damanm72@gmail.com>
2014-11-03 11:53:30 -0500

    add pacman aliases

--- sys/alias.zsh
+++ sys/alias.zsh
@@ -14,6 +14,16 @@ alias chown='chown --preserve-root'
 alias chmod='chmod --preserve-root'
 alias chgrp='chgrp --preserve-root'
 
+# pacman
+alias pac='pacaur --noedit'
+alias pacs='pac -S'
+alias pacr='pac -R'
+alias pacq='pac -Q'
+alias pacu='pac -Syyu'
+alias paco='pac -Qqdt --color=never'
+alias pacoj='pacr $(paco)'
+alias pacad='pac -D --asexplicit'
+
 # misc system commands
 alias ping='ping -c 5'
 alias dmesg='dmesg -HL'
