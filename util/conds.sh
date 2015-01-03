e3aa488c8f29ba542a5a1d2daedef00960ecb718
Octaplex <damanm72@gmail.com>
2015-01-02 22:26:13 -0500

    major changes

--- util/conds.sh
+++
@@ -1,5 +0,0 @@
-declare -A SWITCHES
-
-SWITCHES[MEDIA_KEYS]=$(xmodmap -pke | grep XF86Audio)
-SWITCHES[SLEEP_KEY]=$(xmodmap -pke | grep XF86Sleep)
-SWITCHES[BATTERY]=$(ls /sys/class/power_supply | grep BAT)
